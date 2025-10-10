3de1639982819dd77f8fedc5573f90e0e4d833fe
visniveckijdmitro81-netizen <visniveckijdmitro81@gmail.com>
2025-10-10 20:39:21 +0300

    3d

--- script.js
+++ script.js
@@ -161,3 +161,15 @@ searchInput.addEventListener("input", () => {
   });
 });
 
+const card = document.getElementById("5");
+
+
+// 3D анимация блока
+document.addEventListener('mousemove', function(e) {
+    let dx = e.pageX - window.innerWidth / 2
+    let dy = e.pageY - window.innerHeight / 2
+    let angleX = 20 * dx / window.innerWidth / 2
+    let angleY = 20 * dy / window.innerHeight / 2
+    card.style.transform = `rotateX(${angleY}deg) rotateY(${angleX}deg)`
+})
+
